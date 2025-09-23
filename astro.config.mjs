c9e8fbd51b8ab743bce4b97cd32bbec7fa6103b9
Jun Matsui <info@johncompany.biz>
2025-09-23 21:28:15 +0900

    create sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://happy-m-work.com',
+	trailingSlash: 'always',
 	integrations: [mdx(), tailwind(), sitemap()],
 });
